7a662bf7a88d14c7ff79b50f59b13af7305badac
ITJANG <whalswkd1213@gmail.com>
2025-03-17 08:37:46 +0900

    업데이트

---
+++ 12_io/src/edu/kh/io/pack3/run/ObjectRun.java
@@ -0,0 +1,16 @@
+package edu.kh.io.pack3.run;
+
+import java.io.ObjectStreamClass;
+
+import edu.kh.io.pack3.model.service.ObjectService;
+
+public class ObjectRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ObjectService os = new ObjectService();
+		
+		os.objectInput();
+	}
+
+}
---
+++ 12_io/src/edu/kh/io/pack4/run/CharacterRun.java
@@ -0,0 +1,14 @@
+package edu.kh.io.pack4.run;
+
+import edu.kh.io.pack4.model.service.CharacterService;
+
+public class CharacterRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CharacterService cs = new CharacterService();
+		
+		cs.fileOutput();
+	}
+
+}
